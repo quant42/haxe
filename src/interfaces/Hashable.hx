4d8c648e0ede13a043c6a01435c336689d91de5a
Yann <ga29qal@mytum.de>
2017-05-29 09:36:24 +0200

    some interfaces

---
+++ src/interfaces/Hashable.hx
@@ -0,0 +1,5 @@
+package interfaces;
+
+interface Hashable {
+    public function hashCode():Int;
+}
---
+++ src/interfaces/Numeric.hx
@@ -0,0 +1,5 @@
+package interfaces;
+
+interface Numeric {
+    public function getValue():Float;
+}
