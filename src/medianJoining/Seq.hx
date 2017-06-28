a68cabffe1d48eebe14496a46670e471cc6d3d76
Yann <ga29qal@mytum.de>
2017-06-28 06:45:37 +0200

    MJ/Seqs

--- src/medianJoining/Seq.hx
+++ src/medianJoining/Seq.hx
@@ -1,29 +1,45 @@
 package medianJoining;
 
+import haxe.ds.Vector;
+
 class Seq {
     public var originalSequence:String;
-    public var reducedSequence:String;
     public var names:List<String>;
-
     public var isSample:Bool;
-
-    public var myDeltas:List<Delta>;
 
     public var next:Seq;
     public var prev:Seq;
 
-    public var id:Int;
+    public var reducedSequence:String;
 
-    public var posX:Float;
-    public var posY:Float;
+    public var deltas:List<Delta>;
 
     public var connectedTo:List<Seq>;
-    public var links:List<Seq>;
 
     public inline function new() {
         this.names = new List<String>();
-        this.myDeltas = new List<Delta>();
+        this.deltas = new List<Delta>();
         this.connectedTo = new List<Seq>();
-        this.links = new List<Seq>();
+    }
+
+    public static inline function createSample(names:List<String>, seq:String):Seq {
+        var result:Seq = new Seq();
+        result.names = names;
+        result.originalSequence = seq;
+        return result;
+    }
+    public static inline function createMedian(seq:String):Seq {
+        var result:Seq = new Seq();
+        result.reducedSequence = seq;
+        return result;
+    }
+
+    public inline function createReducedSequence(l:List<Int>):Void {
+        var a:Vector<String> = new Vector<String>(l.length);
+        var i:Int = 0;
+        for(pos in l) {
+            a[i++] = this.originalSequence.charAt(pos);
+        }
+        this.reducedSequence = a.join("");
     }
 }
--- src/medianJoining/Seqs.hx
+++ src/medianJoining/Seqs.hx
@@ -1,19 +1,108 @@
 package medianJoining;
 
+import haxe.ds.Vector;
+
 class Seqs {
     public var first:Seq;
     public var firstMed:Seq;
     public var end:Seq;
     public var length:Int;
 
+    var weights:Vector<Float>;
+    var rweights:Vector<Float>;
+
     public var ipos:List<Int>;
 
-//    public var deltas:List<Delta>;
     public var nextIndex:Int;
 
     public inline function new() {
         ipos = new List<Int>();
-        deltas = new List<List<Float>>;
+    }
+
+    public inline function addSample(names:List<String>,seq:String):Void {
+        var s:Seq = Seq.createSample(names, seq);
+        if(first == null) {
+            first = s;
+        } else {
+            if(first.originalSequence.length != seq.length) {
+                throw "Expected sequence of length " + first.originalSequence.length + " but got sequence of length " + seq.length + "!";
+            }
+            s.prev = end;
+            end.next = s;
+        }
+        end = s;
+        length++;
+    }
+    public inline function addMedian(seq:String):Void {
+        var s:Seq = Seq.createMedian(seq);
+        if(firstMed == null) {
+            firstMed = s;
+        }
+        if(end != null) {
+            s.prev = end;
+            end.next = s;
+        }
+        end = s;
+        length++;
+    }
+    public inline function finishedAddingSamples():Void {
+        // calculate ipos
+        ipos = new List<Int>();
+        for(pos in 0...first.originalSequence.length) {
+            var current:Seq = first;
+            while(current != null) {
+                if(first.originalSequence.charCodeAt(pos) != current.originalSequence.charCodeAt(pos)) {
+                    ipos.add(pos);
+                    break;
+                }
+                current = current.next;
+            }
+        }
+        // call the reduced string calculation for each ipos
+        var current:Seq = first;
+        while(current != null) {
+            current.createReducedSequence(ipos);
+            current = current.next;
+        }
+        // calc rweights
+        if(weights == null) {
+            rweights = new Vector<Float>(ipos.length);
+            for(i in 0...ipos.length) {
+                rweights[i] = 1;
+            }
+        } else {
+            this.rweights = new Vector(ipos.length);
+            var iii:Int = 0;
+            for(e in ipos) {
+                this.rweights[iii++] = weights[e];
+            }
+        }
+    }
+
+    public inline function distStr(s1:String,s2:String):Float {
+        var result:Float = 0.0;
+        for(pos in 0...s1.length) {
+            if(s1.charAt(pos) != s2.charAt(pos)){
+                result += rweights[pos];
+            }
+        }
+        return result;
+    }
+
+    public inline function step1():Void {
+        var deltas:List<Delta> = new List<Delta>();
+        var s1:Seq = first;
+        while(s1 != null) {
+            var s2:Seq = s1.next;
+            while(s2 != null) {
+                var d:Delta = new Delta(s1, s2, distStr(s1.reducedSequence, s2.reducedSequence));
+                deltas.add(d);
+                s1.deltas.add(d);
+                s2.deltas.add(d);
+                s2 = s2.next;
+            }
+            s1 = s1.next;
+        }
     }
 
     public static function main():Void {
